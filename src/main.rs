0510799474c3fda7ee730119134e899209772155
Andreas Giereck Kristiansen <giereck@hotmail.com>
2024-07-27 13:33:08 +0200

    Send response pr. request.

--- src/main.rs
+++ src/main.rs
@@ -9,28 +9,35 @@ fn main() {
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-
         handle_connection(stream);
     }
 }
 
 fn handle_connection(mut stream: TcpStream) {
-    let request = read_request(&mut stream).unwrap();
-    println!("REQUEST:\r\n{:#?}", request);
+    let requests = read_request(&mut stream).unwrap();
+    println!("REQUEST:\r\n{:#?}", requests);
 
-    stream.write_all("+PONG\r\n".as_bytes()).unwrap()
+    for _ in requests {
+        write_response(&stream);
+    }
 }
 
-fn read_request(stream: &mut TcpStream) -> Result<String, String> {
+fn read_request(stream: &mut TcpStream) -> Result<Vec<String>, String> {
     let mut read_buff = [0; 1024];
     match stream.read(&mut read_buff) {
         Ok(bytes_read) => {
             if let Ok(request) = String::from_utf8(read_buff[..bytes_read].to_vec()) {
-                Ok(request)
+                let split = request.split('\n');
+                let requests = split.into_iter().map(|x| x.to_string()).collect();
+                Ok(requests)
             } else {
                 Err(String::from("Received non-UTF8 data."))
             }
         }
         Err(e) => Err(format!("Failed to read bytes from stream: {}", e)),
     }
+}
+
+fn write_response(mut stream: &TcpStream) {
+    stream.write_all("+PONG\r\n".as_bytes()).unwrap()
 }
