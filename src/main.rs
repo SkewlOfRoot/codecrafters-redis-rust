e98c744fcb342908f066372d33eec4813a2e2d8f
Andreas Giereck Kristiansen <giereck@hotmail.com>
2024-07-27 14:13:34 +0200

    Enabled debug info.

--- src/main.rs
+++ src/main.rs
@@ -27,7 +27,7 @@ fn read_request(stream: &mut TcpStream) -> Result<Vec<String>, String> {
     match stream.read(&mut read_buff) {
         Ok(bytes_read) => {
             if let Ok(request) = String::from_utf8(read_buff[..bytes_read].to_vec()) {
-                println!("Incomming request string: {}", request);
+                println!("Incomming request string: {:#?}", request);
                 let split = request.split('\n');
                 let requests = split
                     .into_iter()
