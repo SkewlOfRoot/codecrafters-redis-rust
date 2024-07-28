932ef32d055aa43f018e8e146ffa3bdee19807a0
Andreas Giereck Kristiansen <giereck@hotmail.com>
2024-07-28 23:40:36 +0200

    Parsing the incomming protocol and handling the ECHO command.

--- src/main.rs
+++ src/main.rs
@@ -30,11 +30,13 @@ fn handle_request(stream: &mut TcpStream) -> Result<(), String> {
         if bytes_read == 0 {
             break;
         }
-        if let Ok(request) = String::from_utf8(read_buff[..bytes_read].to_vec()) {
-            println!("Incomming request string: {:#?}", request);
 
-            if request.contains("PING") {
-                write_response(stream);
+        if let Ok(request) = String::from_utf8(read_buff[..bytes_read].to_vec()) {
+            let command = parse_protocol(&request);
+
+            match command {
+                Ok(c) => handle_response(stream, c),
+                Err(e) => println!("{}", e),
             }
         } else {
             return Err(String::from("Received non-UTF8 data."));
@@ -44,8 +46,54 @@ fn handle_request(stream: &mut TcpStream) -> Result<(), String> {
     Ok(())
 }
 
-fn write_response(mut stream: &TcpStream) {
-    let response_text = "+PONG\r\n";
-    println!("RESPONSE: {:#?}", response_text);
-    stream.write_all(response_text.as_bytes()).unwrap()
+fn parse_protocol(protocol_str: &str) -> Result<Command, &'static str> {
+    println!("Incomming request string: {:#?}", protocol_str);
+    let proto_elements: Vec<&str> = protocol_str.split("\r\n").collect();
+    match proto_elements.get(1) {
+        Some(val) => {
+            let val = val.to_lowercase();
+            let command_str = val.as_str();
+            match command_str {
+                "ping" => Ok(Command::Ping),
+                "echo" => {
+                    let message = proto_elements.get(2).expect("Could not get ECHO message.");
+                    Ok(Command::Echo(EchoCommand::new(message)))
+                }
+                _ => Err("Command not recognized."),
+            }
+        }
+        None => Err("No command was found in protocol input."),
+    }
+}
+
+fn handle_response(stream: &mut TcpStream, command: Command) {
+    match command {
+        Command::Ping => write_response(stream, "+PONG\r\n"),
+        Command::Echo(cmd) => {
+            let res = format!("${}\r\n{}\r\n", cmd.message.len(), cmd.message);
+            write_response(stream, &res);
+        }
+    }
+}
+
+fn write_response(mut stream: &TcpStream, response_str: &str) {
+    println!("RESPONSE: {:#?}", response_str);
+    stream.write_all(response_str.as_bytes()).unwrap()
+}
+
+struct EchoCommand {
+    message: String,
+}
+
+enum Command {
+    Ping,
+    Echo(EchoCommand),
+}
+
+impl EchoCommand {
+    fn new(message: &str) -> Self {
+        EchoCommand {
+            message: message.to_string(),
+        }
+    }
 }
